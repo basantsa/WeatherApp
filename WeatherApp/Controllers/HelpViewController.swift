fd71a29d41e7f82af1f228479ce86a2749208114
Basant <basant.sarda@gmail.com>
2017-03-18 14:18:01 +0530

    Added HelpViewController.

---
+++ WeatherApp/Controllers/HelpViewController.swift
@@ -0,0 +1,56 @@
+//
+//  HelpViewController.swift
+//  WeatherApp
+//
+//  Created by Basant Sarda on 18/03/17.
+//  Copyright © 2017 Basant. All rights reserved.
+//
+
+import UIKit
+
+class HelpViewController: BaseViewController, UIWebViewDelegate {
+
+    @IBOutlet weak var webView:UIWebView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let url = Bundle.main.url(forResource: "weatherApp", withExtension: "html")
+        let request = URLRequest(url: url!)
+        webView?.loadRequest(request)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //MARK: - UIWebViewDelegate
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
